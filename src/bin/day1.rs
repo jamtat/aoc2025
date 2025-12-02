f28b9e1fb8c4085a36dc3df65f4a1486a098c12f
Ben Tattersley <ben@jamstudio.local>
2025-12-02 23:13:47 +0000

    day1 part2

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,5 +1,8 @@
 use aoc2025::aoc;
 
+const START: i64 = 50;
+const DIAL_SIZE: i64 = 100;
+
 mod part1 {
     use super::*;
 
@@ -16,13 +19,12 @@ mod part1 {
     }
 
     pub fn calculate(input: &str) -> usize {
-        let mut pos = 50;
+        let mut pos = START;
         let mut zeroes = 0;
-        const DIAL_SIZE: i64 = 100;
 
         for i in input.lines().map(parse_line) {
             pos += i;
-            pos = pos % DIAL_SIZE;
+            pos = pos.rem_euclid(DIAL_SIZE);
             zeroes += (pos == 0) as usize;
         }
 
@@ -40,12 +42,57 @@ mod part1 {
         }
     }
 }
-/*
+
 mod part2 {
+
+    use crate::part1::parse_line;
+
     use super::*;
 
     pub fn calculate(input: &str) -> usize {
-        0
+        let mut pos = START;
+        let mut zeroes = 0;
+
+        for l in input.lines() {
+            let i = parse_line(l);
+            if i == 0 {
+                continue;
+            }
+            let mut points: usize = 0;
+            let full_rotations = (i / DIAL_SIZE).abs() as usize;
+            points += full_rotations;
+
+            let rem = i % DIAL_SIZE;
+
+            if pos != 0 && rem != 0 {
+                let newpos = pos + rem;
+
+                let rem_cross = newpos >= 100 || newpos <= 0;
+                if rem_cross {
+                    points += 1;
+                }
+            }
+
+            pos += i;
+            pos = pos.rem_euclid(DIAL_SIZE);
+
+            zeroes += points;
+
+            #[cfg(test)]
+            if points > 0 {
+                use colored::Colorize;
+
+                println!(
+                    "The dial is rotated {l} to point at {pos}; during the rotation it points at zero {} time{} ({full_rotations} full rotations)",
+                    (&format!("{points}")).bold(),
+                    if points == 1 { ' ' } else { 's' },
+                );
+            } else {
+                println!("The dial is rotated {l} to point at {pos}.");
+            }
+        }
+
+        zeroes
     }
 
     #[cfg(test)]
@@ -55,16 +102,21 @@ mod part2 {
         #[test]
         fn test_example() {
             let input = aoc::example::example_string("day1.txt");
-            assert_eq!(calculate(&input), 0);
+            assert_eq!(calculate(&input), 6);
+        }
+
+        #[test]
+        fn test_more() {
+            assert_eq!(calculate("R1000"), 10);
         }
     }
 }
-*/
+
 fn main() {
     let cli = aoc::cli::parse();
 
     let input = cli.input_string();
 
     println!("Part 1: {}", part1::calculate(&input));
-    // println!("Part 2: {}", part2::calculate(&input));
+    println!("Part 2: {}", part2::calculate(&input)); // 5978
 }
