f61432a880ef12970f46a143cd38103c112b8e01
Ben Tattersley <ben@jamstudio.local>
2025-12-03 16:20:28 +0000

    Fancy generic digits impl

--- src/quant.rs
+++ src/quant.rs
@@ -99,3 +99,90 @@ impl_rem_euclid!(i32);
 impl_rem_euclid!(i64);
 impl_rem_euclid!(i128);
 impl_rem_euclid!(isize);
+
+pub trait ILog10 {
+    fn ilog10(self) -> u32;
+}
+macro_rules! impl_ilog10 {
+    ($typ:ty) => {
+        impl ILog10 for $typ {
+            fn ilog10(self) -> u32 {
+                self.ilog10()
+            }
+        }
+    };
+}
+
+impl_ilog10!(u8);
+impl_ilog10!(u16);
+impl_ilog10!(u32);
+impl_ilog10!(u64);
+impl_ilog10!(u128);
+impl_ilog10!(usize);
+impl_ilog10!(i8);
+impl_ilog10!(i16);
+impl_ilog10!(i32);
+impl_ilog10!(i64);
+impl_ilog10!(i128);
+impl_ilog10!(isize);
+
+pub trait UnsignedAbs {
+    type Output;
+    fn unsigned_abs(self) -> Self::Output;
+}
+macro_rules! impl_uabs_u {
+    ($typ:ty) => {
+        impl UnsignedAbs for $typ {
+            type Output = $typ;
+            fn unsigned_abs(self) -> Self::Output {
+                self
+            }
+        }
+    };
+}
+impl_uabs_u!(u8);
+impl_uabs_u!(u16);
+impl_uabs_u!(u32);
+impl_uabs_u!(u64);
+impl_uabs_u!(u128);
+impl_uabs_u!(usize);
+
+macro_rules! impl_uabs_i {
+    ($typ:ty, $typ2:ty) => {
+        impl UnsignedAbs for $typ {
+            type Output = $typ2;
+            fn unsigned_abs(self) -> Self::Output {
+                self.unsigned_abs()
+            }
+        }
+    };
+}
+impl_uabs_i!(i8, u8);
+impl_uabs_i!(i16, u16);
+impl_uabs_i!(i32, u32);
+impl_uabs_i!(i64, u64);
+impl_uabs_i!(i128, u128);
+impl_uabs_i!(isize, usize);
+
+pub fn digits<T, U>(x: T) -> u32
+where
+    T: Copy + UnsignedAbs<Output = U>,
+    U: Copy + Eq + ILog10 + NumConsts,
+{
+    let x = x.unsigned_abs();
+    if x == U::ZERO { 1 } else { x.ilog10() + 1 }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_digits() {
+        assert_eq!(digits(0), 1);
+        assert_eq!(digits(1), 1);
+        assert_eq!(digits(-1), 1);
+        assert_eq!(digits(100), 3);
+        assert_eq!(digits(-100), 3);
+    }
+}
