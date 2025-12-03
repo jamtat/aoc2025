7ff8643f4ff2b142fd90ef192486d76e7d740e97
Ben Tattersley <ben@jamstudio.local>
2025-12-03 16:05:12 +0000

    day3 part2

--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -1,19 +1,38 @@
 use aoc2025::aoc;
 
-pub struct Bank(Vec<u8>);
+fn max_concat_digits(xs: &[usize], count: u32) -> usize {
+    if count == 0 {
+        return 0;
+    }
 
-impl Bank {
-    pub fn max_joltage(&self) -> u8 {
-        let mut max = 0;
+    if (count as usize) > xs.len() {
+        panic!("Items not long enough to find max concat digits");
+    }
 
-        for (i, bat1) in self.0[..self.0.len() - 1].iter().enumerate() {
-            for bat2 in self.0[i + 1..].iter() {
-                let joltage = bat1 * 10 + bat2;
-                max = max.max(joltage);
+    let mut max_digit = 0;
+    let mut max_ptr = 0;
+    let last_item_allowed = xs.len() - (count as usize);
+
+    for (i, &x) in xs[..=last_item_allowed].iter().enumerate() {
+        if x > max_digit {
+            max_ptr = i;
+            max_digit = x;
+            if x == 9 {
+                break;
             }
         }
+    }
 
-        max
+    10usize.pow(count - 1) * max_digit + max_concat_digits(&xs[max_ptr + 1..], count - 1)
+}
+
+pub struct Bank(Vec<usize>);
+
+impl std::ops::Deref for Bank {
+    type Target = [usize];
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
     }
 }
 
@@ -21,7 +40,7 @@ impl std::str::FromStr for Bank {
     type Err = ();
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        Ok(Bank(s.bytes().map(|b| b - b'0').collect()))
+        Ok(Bank(s.bytes().map(|b| (b - b'0') as usize).collect()))
     }
 }
 
@@ -35,7 +54,7 @@ mod part1 {
     pub fn calculate(input: &str) -> usize {
         parse_input(input)
             .iter()
-            .map(|bank| bank.max_joltage() as usize)
+            .map(|bank| max_concat_digits(bank, 2))
             .sum()
     }
 
@@ -50,12 +69,15 @@ mod part1 {
         }
     }
 }
-/*
+
 mod part2 {
     use super::*;
 
     pub fn calculate(input: &str) -> usize {
-        0
+        parse_input(input)
+            .iter()
+            .map(|bank| max_concat_digits(bank, 12))
+            .sum()
     }
 
     #[cfg(test)]
@@ -65,16 +87,16 @@ mod part2 {
         #[test]
         fn test_example() {
             let input = aoc::example::example_string("day3.txt");
-            assert_eq!(calculate(&input), 0);
+            assert_eq!(calculate(&input), 3121910778619);
         }
     }
 }
-*/
+
 fn main() {
     let cli = aoc::cli::parse();
 
     let input = cli.input_string();
 
     println!("Part 1: {}", part1::calculate(&input));
-    // println!("Part 2: {}", part2::calculate(&input));
+    println!("Part 2: {}", part2::calculate(&input));
 }
