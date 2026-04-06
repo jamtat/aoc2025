30333335283b81f1a743a83118d3e8e0bc60d0bc
Ben <ben@jambook-neo.local>
2026-04-06 20:34:10 +0100

    Day 5 Part 2

--- src/bin/day5.rs
+++ src/bin/day5.rs
@@ -1,7 +1,9 @@
 use std::str::FromStr;
 
 use aoc2025::aoc;
+use itertools::Itertools;
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 struct Fresh {
     start: u64,
     end: u64,
@@ -10,6 +12,37 @@ struct Fresh {
 impl Fresh {
     pub fn is_fresh(&self, ingredient: u64) -> bool {
         ingredient >= self.start && ingredient <= self.end
+    }
+
+    pub fn len(&self) -> u64 {
+        (self.end - self.start) + 1
+    }
+
+    pub fn intersects(&self, other: &Self) -> bool {
+        !(self.end < other.start || self.start > other.end)
+    }
+
+    pub fn intersection(&self, other: &Self) -> Option<Self> {
+        self.intersects(other).then(|| Self {
+            start: self.start.min(other.start),
+            end: self.end.max(other.end),
+        })
+    }
+
+    pub fn collapse(ranges: &[Self]) -> Vec<Self> {
+        let sorted: Vec<Self> = ranges.iter().cloned().sorted().collect();
+
+        let mut out = vec![sorted[0]];
+        for range in sorted.into_iter().skip(1) {
+            if let Some(intersected) = range.intersection(out.last().unwrap()) {
+                out.pop();
+                out.push(intersected);
+            } else {
+                out.push(range);
+            }
+        }
+
+        out
     }
 }
 
@@ -89,12 +122,15 @@ mod part1 {
         }
     }
 }
-/*
+
 mod part2 {
     use super::*;
 
-    pub fn calculate(input: &str) -> usize {
-        0
+    pub fn calculate(input: &str) -> u64 {
+        let input: Input = input.parse().unwrap();
+        let fresh_ranges = Fresh::collapse(&input.fresh_ranges);
+
+        fresh_ranges.iter().map(Fresh::len).sum()
     }
 
     #[cfg(test)]
@@ -104,16 +140,16 @@ mod part2 {
         #[test]
         fn test_example() {
             let input = aoc::example::example_string("day5.txt");
-            assert_eq!(calculate(&input), 0);
+            assert_eq!(calculate(&input), 14);
         }
     }
 }
-*/
+
 fn main() {
     let cli = aoc::cli::parse();
 
     let input = cli.input_string();
 
     println!("Part 1: {}", part1::calculate(&input));
-    // println!("Part 2: {}", part2::calculate(&input));
+    println!("Part 2: {}", part2::calculate(&input));
 }
