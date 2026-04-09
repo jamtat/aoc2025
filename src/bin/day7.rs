2f7f357ebf6ef88265bf3923ca457fe7b7a6c547
Ben Tattersley <ubershinysheep@gmail.com>
2026-04-09 22:21:29 +0100

    Naive impl that takes too long to run

--- src/bin/day7.rs
+++ src/bin/day7.rs
@@ -68,14 +68,40 @@ mod part2 {
         })
     }
 
+    #[allow(unused_assignments, unused_variables)]
     pub fn calculate(input: &str) -> usize {
+        let max_y = input.lines().count() - 1;
         let splitters: HashSet<Point> = find_coords(input, b'^').collect();
-        let mut visited = HashSet::<Point>::new();
         let mut queue: VecDeque<Point> = find_coords(input, b'S').collect();
-        println!("queue={queue:?}");
-        println!("splitters={splitters:?}");
+        // let mut visited: HashSet<Point> = HashSet::new();
+        // println!("queue={queue:?}");
+        // println!("splitters={splitters:?}");
         let mut routes = 0;
+        let mut its = 0;
 
+        while let Some(point) = queue.pop_front() {
+            its += 1;
+            // if !visited.insert(point) {
+            //     // If we have already visited this point then we've arrived at it by a different route before
+            //     routes += 1;
+            //     continue;
+            // }
+            let next = point.down();
+
+            if next.y == max_y {
+                // Reached the end so record a route
+                routes += 1;
+            } else if splitters.contains(&next) {
+                if let Some(left) = next.left() {
+                    queue.push_back(left);
+                }
+                queue.push_back(next.right());
+            } else {
+                queue.push_back(next);
+            }
+        }
+        #[cfg(test)]
+        dbg!(its);
         routes
     }
 
@@ -86,7 +112,7 @@ mod part2 {
         #[test]
         fn test_example() {
             let input = aoc::example::example_string("day7.txt");
-            assert_eq!(calculate(&input), 0);
+            assert_eq!(calculate(&input), 40);
         }
     }
 }
@@ -97,5 +123,5 @@ fn main() {
     let input = cli.input_string();
 
     println!("Part 1: {}", part1::calculate(&input));
-    // println!("Part 2: {}", part2::calculate(&input));
+    println!("Part 2: {}", part2::calculate(&input));
 }
